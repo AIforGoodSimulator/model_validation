4f71f790d90664c6146a2f2c730e1e17ab99a791
Alex Gowler <33961417+A14x@users.noreply.github.com>
2020-10-27 14:33:52 +0000

    Update cm_compare_validation_output_plots.py

--- cm_compare_validation_output_plots.py
+++ cm_compare_validation_output_plots.py
@@ -7,8 +7,6 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-#To remove
-import pprint as pp
 
 pd.set_option("display.max_rows", None, "display.max_columns", None)
 
